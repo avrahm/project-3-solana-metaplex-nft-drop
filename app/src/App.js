b3536fc3662704b00f255097c8d08222c55854cf
Avrahm <avrahmk@me.com>
2021-12-12 14:13:37 -0500

    project 3 - section 1 - solana nft drop - connect wallet

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,6 +7,41 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  // check if wallet is connected
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom Wallet Found!');
+          const response = await solana.connect({ onlyIfTrusted: true });
+
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          );
+          // setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("Phantom not found! You'll need Phantom to get started");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // Check if wallet is connected
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    // Run on load
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+
   return (
     <div className="App">
       <div className="container">
